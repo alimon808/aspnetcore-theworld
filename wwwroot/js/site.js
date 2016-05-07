116a155e36f9dc983d71f1288494290ac45f5365
Adrian Limon <me@adrianlimon.com>
2016-05-07 13:43:22 -0700

    js - immediately invoked function expression (iife)

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿
+(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Shawn Wildermuth";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style["background-color"] = "#888";
+    };
+
+    main.onmouseleave = function () {
+        main.style["background-color"] = "";
+    };
+})();
