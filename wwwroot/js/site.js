4bc02465f8ce712272c30f19eafbe018ae7ebd33
Adrian Limon <me@adrianlimon.com>
2016-05-07 14:19:04 -0700

    end of javascript module

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,32 @@
 ﻿
 (function () {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Shawn Wildermuth";
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style["background-color"] = "#888";
-    };
+    //var ele = $("#username");
+    //ele.text("Shawn Wildermuth");
 
-    main.onmouseleave = function () {
-        main.style["background-color"] = "";
-    };
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color:#888;";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style ="";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
 })();
