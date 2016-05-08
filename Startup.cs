1046a691e6253d67efb3bb688efe38c799d721e4
Adrian Limon <me@adrianlimon.com>
2016-05-08 16:36:45 -0700

    enable mvc

--- Startup.cs
+++ Startup.cs
@@ -15,18 +15,14 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseDefaultFiles();
             app.UseStaticFiles();
-
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
+            app.UseMvc();            
         }
 
         // Entry point for the application.
