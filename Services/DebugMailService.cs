1fbc3fb551e45bfce0c1b85b475923f6896b32c4
Adrian Limon <me@adrianlimon.com>
2016-05-08 17:53:18 -0700

    implmented contact form

---
+++ Services/DebugMailService.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace TheWorld.Services
+{
+    public class DebugMailService : IMailService
+    {
+        public bool SendMail(string to, string from, string subject, string message)
+        {
+            Debug.WriteLine($"Sending mail: To: {to}, Subject: {subject}, Message: {message}");
+            return true;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNet.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using TheWorld.Services;
 
 namespace TheWorld
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+#if DEBUG
+            services.AddScoped<IMailService, DebugMailService>();
+#else
+            services.AddScoped<IMailService, MailService>();
+#endif
         }
 
         public void Configure(IApplicationBuilder app)
