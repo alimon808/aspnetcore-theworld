889d5303f0543601cbab79a51ea844afa4f4f015
Adrian Limon <me@adrianlimon.com>
2016-05-10 23:10:07 -0700

    implemented repository pattern

---
+++ Models/IWorldRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.Models
+{
+    public interface IWorldRepository
+    {
+        IEnumerable<Trip> GetAllTrips();
+        IEnumerable<Trip> GetAllTripsWithStops();
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace TheWorld
                 .AddDbContext<WorldContext>();
 
             services.AddTransient<WorldContextSeedData>();
-
+            services.AddScoped<IWorldRepository, WorldRepository>();
 
 #if DEBUG
             services.AddScoped<IMailService, DebugMailService>();
