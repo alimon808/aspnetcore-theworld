ff4e7df70ddc58bbd7508ab4b6666beac2eaff81
Adrian Limon <me@adrianlimon.com>
2016-05-14 14:02:41 -0700

    created angular trips view and editor view

---
+++ wwwroot/js/tripEditorController.js
@@ -0,0 +1,28 @@
+﻿
+(function () {
+    "use strict";
+
+    angular.module("app-trips")
+           .controller("tripEditorController", tripEditorController);
+
+    function tripEditorController($routeParams, $http) {
+        var vm = this;
+
+        vm.tripName = $routeParams.tripName;
+        vm.stops = [];
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+             .then(function (response) {
+                 angular.copy(response.data, vm.stops);
+             }, function (err) {
+                 vm.errorMessage = "Failed to load stops";
+             })
+             .finally(function () {
+                 vm.isBusy = false;
+             });
+
+    }
+
+})();
